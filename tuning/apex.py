a9472feaa869ef5d66c569e0aa0ff65ac174559a
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-06-27 20:22:01 -0400

    debug

--- tuning/apex.py
+++ tuning/apex.py
@@ -5,15 +5,14 @@ import numpy as np
 
 space = {
         "lr": hp.loguniform("lr", np.log(1e-5), np.log(1e-3)),
-        "n_step": scope.int(hp.quniform("num_sgd_iter", 3, 12, 1)),
-        "gamma": hp.choice("gamma", [0.99, 0.999]),
+        "n_step": scope.int(hp.quniform("n_step", 3, 12, 1)),
+        "gamma": hp.choice("gamma", [0.99, 0.999])
     }
 current_best_params = [
     {
         "lr": 1e-4,
         "n_step": 3,
-        "gamma": 0.99,
-
+        "gamma": 0.99
     }
 ]
 
