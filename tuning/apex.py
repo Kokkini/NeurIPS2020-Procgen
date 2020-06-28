6c8d46f602a9f85b33564767ec4f6eca6c37ae29
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-06-27 21:02:12 -0400

    apex rainbow tuning

--- tuning/apex.py
+++ tuning/apex.py
@@ -5,14 +5,16 @@ import numpy as np
 
 space = {
         "lr": hp.loguniform("lr", np.log(1e-5), np.log(1e-3)),
+        "target_network_update_freq": scope.int(hp.loguniform("target_network_update_freq", np.log(1e4), np.log(1e5))),
         "n_step": scope.int(hp.quniform("n_step", 3, 12, 1)),
-        "gamma": hp.choice("gamma", [0.99, 0.999])
+        "num_atoms": scope.int(hp.quniform("num_atoms", 20, 100, 1))
     }
 current_best_params = [
     {
         "lr": 1e-4,
-        "n_step": 3,
-        "gamma": 0.99
+        "target_network_update_freq": 32000,
+        "num_atoms": 51,
+        "n_step": 3
     }
 ]
 
