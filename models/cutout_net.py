917420399f65cb0f3c1f98943a5c653eac34965f
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-07-01 19:36:30 -0400

    delete dockerfile

--- models/cutout_net.py
+++ models/cutout_net.py
@@ -1,6 +1,7 @@
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.models import ModelCatalog
+import tensorflow_addons as tfa
 
 tf = try_import_tf()
 
