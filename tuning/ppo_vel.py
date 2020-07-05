64a511aa43f7233f561a5ada52d100ff5f6028bd
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-07-04 20:26:12 -0400

    tune temperature

--- tuning/ppo_vel.py
+++ tuning/ppo_vel.py
@@ -8,7 +8,10 @@ space = {
         "vf_clip_param": hp.loguniform("vf_clip_param", np.log(0.04), np.log(1)),
 		"entropy_coeff": hp.loguniform("entropy_coeff", np.log(1e-3), np.log(1e-1)),
 		"vf_loss_coeff": hp.loguniform("vf_loss_coeff", np.log(0.1), np.log(2.5)),
-		"num_sgd_iter": scope.int(hp.quniform("num_sgd_iter", 2, 6, 1))
+		"num_sgd_iter": scope.int(hp.quniform("num_sgd_iter", 2, 6, 1)),
+        "exploration_config":{
+            "temperature": hp.loguniform("temperature", np.log(0.5), np.log(5))
+        }
     }
 current_best_params = [
     {
@@ -16,7 +19,10 @@ current_best_params = [
         "vf_clip_param": 0.1,
         "entropy_coeff": 0.0019,
         "num_sgd_iter": 2,
-        "vf_loss_coeff": 0.45
+        "vf_loss_coeff": 0.45,
+        "exploration_config":{
+            "temperature": 1
+        }
     }
 ]
 
