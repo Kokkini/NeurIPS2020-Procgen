2865dd7219b6d5c25ac6bf1a20156f3d09f1faa1
Kokkini <trannhatquang1104@gmail.com>
2020-08-18 06:03:50 +0000

    tunning for bigfish

--- tuning/impala_round1.py
+++ tuning/impala_round1.py
@@ -5,13 +5,18 @@ import numpy as np
 
 space = {
         "lr": hp.loguniform("lr", np.log(1e-5), np.log(1e-3)),
-        "gamma": hp.uniform("gamma", 0.9, 1),
-
+        "gamma": hp.uniform("gamma", 0.98, 1),
+        "entropy_coeff": hp.loguniform("entropy_coeff", np.log(1e-2), np.log(1e-1)),
+        "clip_rewards": hp.choice("clip_rewards", [True, False]),
+        "vf_clip_param": hp.loguniform("vf_clip_param", np.log(1e-1), np.log(1e1)),
     }
 current_best_params = [
     {
         "lr": 0.0006,
         "gamma": 0.9,
+        "entropy_coeff": 0.01,
+        "clip_rewards": True,
+        "vf_clip_param": 0.4,
     }
 ]
 
