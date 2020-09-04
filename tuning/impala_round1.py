927ac2eb9f729e275fe1a9266b0d29d3791a57e3
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-09-04 01:07:00 -0400

    tune networksize

--- tuning/impala_round1.py
+++ tuning/impala_round1.py
@@ -4,20 +4,12 @@ from hyperopt.pyll.base import scope
 import numpy as np
 
 space = {
-        "lr": hp.loguniform("lr", np.log(1e-5), np.log(1e-3)),
-        "gamma": hp.uniform("gamma", 0.98, 1),
-        "entropy_coeff": hp.loguniform("entropy_coeff", np.log(1e-2), np.log(1e-1)),
-        "clip_rewards": hp.choice("clip_rewards", [True, False]),
-        "vf_clip_param": hp.loguniform("vf_clip_param", np.log(1e-1), np.log(1e1)),
+        "lr": hp.loguniform("lr", np.log(4e-5), np.log(1e-3)),
+        "entropy_coeff": hp.loguniform("entropy_coeff", np.log(3e-3), np.log(1e-1)),
+        "num_sgd_iter": hp.choice("num_sgd_iter",[2,3,4,5]),
+        "vf_loss_coeff": hp.uniform("vf_loss_coeff", 0.1, 1),
     }
 current_best_params = [
-    {
-        "lr": 0.0006,
-        "gamma": 0.9,
-        "entropy_coeff": 0.01,
-        "clip_rewards": True,
-        "vf_clip_param": 0.4,
-    }
 ]
 
 algo = HyperOptSearch(
