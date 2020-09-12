d5089f179a5c17d3e0f927deb54b43a3ea0e72dc
Kokkini <trannhatquang1104@gmail.com>
2020-09-12 05:41:05 +0000

    debug

--- tuning/subtract_and_size.py
+++ tuning/subtract_and_size.py
@@ -3,10 +3,11 @@ from ray.tune.suggest.hyperopt import HyperOptSearch
 from hyperopt.pyll.base import scope
 import numpy as np
 
+sgd_minibatch_size = hp.choice("sgd_minibatch_size", [256, 512])
 space = {
-        "lr": hp.loguniform("lr", np.log(1e-5), np.log(6e-4)),
+        "lr": hp.loguniform("lr", np.log(2e-5), np.log(3e-4)) * (sgd_minibatch_size/256),
         "vf_loss_coeff": hp.loguniform("vf_loss_coeff", np.log(5e-2), np.log(1)),
-        "sgd_minibatch_size": hp.choice("sgd_minibatch_size", [256, 512])
+        "sgd_minibatch_size": sgd_minibatch_size,
   }
 current_best_params = [
 	{
