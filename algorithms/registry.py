24c4bf9f2c9c942490ebb2dff0c7f03bf841b56e
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-10-17 01:42:37 -0400

    add fast head

--- algorithms/registry.py
+++ algorithms/registry.py
@@ -34,6 +34,10 @@ def _import_beta_vae_ppo_test():
     from .beta_vae_test.ppo import BetaVaePPOTrainerTest
     return BetaVaePPOTrainerTest
 
+def _import_fast_head():
+    from .fast_head.ppo import FastHeadTrainer
+    return FastHeadTrainer
+
 
 CUSTOM_ALGORITHMS = {
     "custom/CustomRandomAgent": _import_custom_random_agent,
@@ -41,4 +45,5 @@ CUSTOM_ALGORITHMS = {
     "MyPPO": _import_my_ppo,
     "BetaVaePPO": _import_beta_vae_ppo,
     "BetaVaePPOTest": _import_beta_vae_ppo_test,
+    "FastHead": _import_fast_head,
 }
